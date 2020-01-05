c964957761f7ec15fb9feb786b510b0e83527f65
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2020-01-05 16:17:53 +0530

    Update userOperations.js

--- 5Jan2020/db/helpers/userOperations.js
+++ 5Jan2020/db/helpers/userOperations.js
@@ -5,7 +5,7 @@ const sendgrid = require('nodemailer-sendgrid-transport');
 
 const transporter = nodemailer.createTransport(sendgrid({
     auth:{
-        api_key:'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'
+        api_key:''
     }
 }));
 
